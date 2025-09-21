556bdceda7fca866d4ce99b91e755b7b0b269893
n30f0x <87524177+n30f0x@users.noreply.github.com>
2025-09-21 04:21:31 +0200

    Building, FS flags refactor. (#6)
    * Building, FS flags refactor.
    Introduce proper prefix eval.
    Prep for foreign OS porting.
    ToDo: fixup definitions.
    * Resolve conflicts, fix messages.
    
         M README.md
         M Makefile.in
         M configure.ac
         M src/args.c
         M src/main.c
         M src/os.c
         M src/os.h
         M src/errors.c

--- src/os.h
+++ src/os.h
@@ -1,26 +1,14 @@
 // autoconf
-// #include <config.h>
+#include "../config.h"
 // per os libs and defines
 #ifdef __APPLE__
 
   #include <sys/syslimits.h>
   #include <sys/_types/_pid_t.h>
-
-  #define PLACEHOLDER "XXXXXXXXXXXXXXXX"
-  #define PLACEHOLDERLEN 0
-
   // copyfiles
   #include <copyfile.h>
 
 #elif __linux__
-
-  #define _GNU_SOURCE
-  #define PLACEHOLDER "XXXXXX"
-  #define PLACEHOLDERLEN 6
-  #define O_EXLOCK F_EXLCK
-  #define PATH_MAX 1024
-  #define MAX_INPUT 1024
-  #define BUFSIZE 4096*1000
 
   // copyfiles
   #include <sys/sendfile.h>
@@ -31,18 +19,8 @@
 
   #include <stdlib.h>
   #include <stdint.h>
-
   #include <sys/stat.h>
 
- // || defined (__BSD__)
- // todo: bsd support
-  #define TMPDIR "/tmp"
-  #define HOME "HOME="
-  #define PATH "PATH="
-#else
-  #define TMPDIR 00
-  #define HOME 00
-  #define PATH 00
 #endif
 
 extern int debug;
